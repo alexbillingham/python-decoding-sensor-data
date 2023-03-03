5afb1d302863a805dedf86c0c34072d64562444e
Alex Billingham <alexbillingham@dsgroup.uk.com>
2023-03-03 11:34:32 +0000

    module_1

---
+++ sensor/load_data.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import csv
+
+
+def load_sensor_data() -> list:
+    sensor_data = []
+    # Load all csv files from datasets folder
+    sensor_files = glob.glob(os.path.join(os.getcwd(), "datasets", "*.csv"))
+    for sensor_file in sensor_files:
+        with open(file=sensor_file) as file_data:
+            data_reader = csv.DictReader(file_data, delimiter=",")
+            for data_row in data_reader:
+                sensor_data.append(data_row)
+
+    return sensor_data
+
+
+if __name__ == "__main__":
+    load_sensor_data()
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,5 @@
 # Runner script for all modules
-
+import load_data
 
 ##############################
 # Do not remove these two lines
@@ -9,6 +9,7 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+print(f"Loaded records: {len(load_data.load_sensor_data())}")
 
 # Module 2 code here:
 
